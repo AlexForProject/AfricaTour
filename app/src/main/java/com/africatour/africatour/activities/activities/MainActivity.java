3e27710336e23c465613f6d5aa4a6b7d46ae5bb1
ILeMH <schanne.alexandre@gmail.com>
2017-07-12 23:40:44 +0200

    test retrofit + authentification

--- app/src/main/java/com/africatour/africatour/activities/activities/MainActivity.java
+++ app/src/main/java/com/africatour/africatour/activities/activities/MainActivity.java
@@ -8,6 +8,7 @@ import android.provider.MediaStore;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v4.app.ShareCompat;
+import android.support.v4.widget.ListViewAutoScrollHelper;
 import android.view.View;
 import android.support.design.widget.NavigationView;
 import android.support.v4.view.GravityCompat;
@@ -17,11 +18,15 @@ import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.ListView;
 import android.widget.TextView;
 
 import com.africatour.africatour.R;
+import com.africatour.africatour.activities.activities.retrofit.RetrofitBuilder;
 
 import org.w3c.dom.Text;
+
+import java.io.IOException;
 
 public class MainActivity extends GlobalForSettings
         implements NavigationView.OnNavigationItemSelectedListener {
@@ -33,7 +38,11 @@ public class MainActivity extends GlobalForSettings
 
         //************************************************
 
-        TextView testbdd = (TextView) findViewById(R.id.testbdd);
+        try {
+            RetrofitBuilder test = new RetrofitBuilder(this, "root", "2588");
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
 
 
         //************************************************
--- app/src/main/java/com/africatour/africatour/activities/activities/retrofit/RetrofitBuilder.java
+++ app/src/main/java/com/africatour/africatour/activities/activities/retrofit/RetrofitBuilder.java
@@ -1,14 +1,21 @@
 package com.africatour.africatour.activities.activities.retrofit;
 
-
+import android.content.Context;
 import android.support.v7.app.AppCompatActivity;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+import android.widget.Toast;
 
+import com.africatour.africatour.R;
 import com.africatour.africatour.activities.activities.Bdd.Client;
+import com.africatour.africatour.activities.activities.MainActivity;
 
+import java.io.IOException;
 import java.util.List;
-
 import okhttp3.OkHttpClient;
 import retrofit2.Call;
+import retrofit2.Callback;
+import retrofit2.Response;
 import retrofit2.Retrofit;
 import retrofit2.converter.gson.GsonConverterFactory;
 
@@ -17,24 +24,54 @@ import retrofit2.converter.gson.GsonConverterFactory;
  * Created by Alexandre on 10/07/2017.
  */
 
-public class RetrofitBuilder extends AppCompatActivity {
-    private final String BASE_URL = "http://10.0.2.2:8081/";
+public class RetrofitBuilder extends AppCompatActivity{
+    private static final String BASE_URL = "http://10.0.2.2:8081";
+    Context ctx;
+    String user;
+    String password;
 
-    OkHttpClient.Builder httpClient = new OkHttpClient.Builder();
+    public RetrofitBuilder(){}
 
-    Retrofit.Builder builder =
-            new Retrofit.Builder()
-                    .baseUrl(BASE_URL)
-                    .addConverterFactory(
-                            GsonConverterFactory.create()
-                    );
+    public RetrofitBuilder(final Context ctx, String user, String password) throws IOException {
+        this.ctx = ctx;
+        this.password = password;
+        this.user = user;
+        OkHttpClient.Builder httpClient = new OkHttpClient.Builder()
+                .addInterceptor(new Authentification(user, password));
 
-    Retrofit retrofit = builder.client(httpClient.build())
-            .build();
+        final Retrofit.Builder builder =
+                new Retrofit.Builder()
+                        .baseUrl(BASE_URL)
+                        .addConverterFactory(
+                                GsonConverterFactory.create())
+                        ;
 
-    Requetes service = retrofit.create(Requetes.class);
+        Retrofit retrofit =
+                builder
+                        .client(
+                                httpClient.build()
+                        )
+                        .build();
 
-    Call<List<Client>> call = service.getClients();
+        Requetes requetes = retrofit.create(Requetes.class);
 
+        Call<List<Client>> call = requetes.getClients();
+
+        call.enqueue(new Callback<List<Client>>() {
+            @Override
+            public void onResponse(Call<List<Client>> call, Response<List<Client>> response) {
+                ListView testbdd = (ListView) findViewById(R.id.testbdd);
+                List<Client> client = response.body();
+                ArrayAdapter<Client> adapter = new ArrayAdapter<Client>(getApplicationContext(), R.layout.adapter, client);
+                testbdd.setAdapter(adapter);
+            }
+
+            @Override
+            public void onFailure(Call<List<Client>> call, Throwable t) {
+                CharSequence erreur = t.getMessage();
+                Toast.makeText(ctx, erreur, Toast.LENGTH_LONG).show();
+            }
+        });
+    }
 }
 
